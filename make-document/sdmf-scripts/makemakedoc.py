05ff1069b4c5b08a4f11f37189636f1e38863123
Trevor <tkmday@uw.edu>
2016-06-01 17:27:45 -0700

    Major refactor into functions.

--- make-document/sdmf-scripts/makemakedoc.py
+++ make-document/sdmf-scripts/makemakedoc.py
@@ -17,7 +17,7 @@ leadingdot=re.compile("^\.")            # looking for ^. (.PHONY, .SECONDARY)
 leadinghash=re.compile("^#")            # looking for comment lines, ^#
 
 
-def save_array( array, filen ):
+def save_array(array, filen):
     """Save the array to the file, or save a placeholder if it is empty."""
 
     if len(array) is not 0:
@@ -30,27 +30,33 @@ def save_array( array, filen ):
             writer = csv.writer(F, delimiter=";", lineterminator='\n')
             writer.writerow("-;None found;-".split(';'))
 
+def check_and_get_comment(startat, spliton):
+    """Get all the comment lines, or return a filler message."""
+    if spliton in linewise[startat - 1]:   
+        comment_list=[]
+        inc=1
+      
+        while spliton in linewise[startat - inc]:
+            comment_list.append(linewise[startat - inc][3::])
+            inc += 1
 
-def get_ml_comments( lines, startat, spliton ):
-    """Get all the comment lines preceding a target"""
-
-    comment_list=[]
-    inc=1
-    
-    while spliton in lines[startat - inc]:
-        comment_list.append(lines[startat - inc][3::])
-        inc += 1
-
-    comment_list.reverse()
-    comment=' '.join(comment_list)
-    return comment
+        comment_list.reverse()
+        comment=' '.join(comment_list)
+        return comment
+    else:
+        return "No comment supplied"
 
 
-#def check_and_get_comment ( lines, startat, checkfor ):
-#    if checkfor in line[startat - 1]:
-#        return(get_ml_comments(linewise, i, checkfor + " *"))
-#    else:
-#        return("No comment supplied")
+def add_to_array(array, new):
+    if array is not None:
+        if len(array) is 0:
+            array = new
+        else:
+            array = np.concatenate([array, np.array(new)])
+        
+        return array
+    else:
+        sys.exit("Array not initialized.")
 
 
 files=sys.argv
@@ -67,42 +73,27 @@ for f in files:
     linewise=filter(None, contents.splitlines()) # remove blank lines
 
     fbn = os.path.basename(f)
-    # escape _ in fbn
-    fbn = re.sub("_", "-", fbn)
 
     for i in range(0, len(linewise)):
         line=linewise[i]
 
         ## GET VARIABLES
         if varmatch.match(line):
-            # get the names of the variables
-            variable=line.rsplit('=')[0]
-            
-            # get the comment if there is any
-            if '#!' in linewise[i-1]:
-                comment = get_ml_comments(linewise, i, "#! ")
-#                comment = "foo"
-            else:
-                comment = "No comment supplied."
+            variable=line.rsplit('=')[0]            
+            comment = check_and_get_comment(i, "#!")
             
             # rsplit works R2L so to get what follows the first '=',
             # we have to reverse, split, then take the first element of that array,
             # then reverse it so we have the result the right way round.
             definition=line[::-1].rsplit('=', 1)[0][::-1]
-        
-            arr = np.array([[variable, definition, comment, fbn]])
 
-            if len(variables) is 0:
-                variables = arr
-            else:
-                variables = np.concatenate((variables, arr))
-                    
+            variables = add_to_array(variables, [[variable, definition, comment, fbn]])
+
         # GET (INTERACTIVE) TARGETS
         if ".PHONY:" in line:
             if len(phonies) is 0:
                 phonies = line.split()
                 del phonies[0]
-                # print phonies
             else:
                 phonies_temp = line.split()
                 del phonies_temp[0]
@@ -112,28 +103,17 @@ for f in files:
         if ':' in line and '\t' not in line and not leadinghash.match(line):
             target=line.rsplit(':', 1)[0]
             
+            # if this isn't the .PHONY line and we're not exporting a variable
             if not leadingdot.match(target) and not "export" in target:
                 if target in phonies:
-                    if "#?" in linewise[i-1]:
-                        comment = get_ml_comments(linewise, i, "#? ")
-                    else:
-                        comment="No comment supplied."
-                    
-                    temp_arr = np.array([[target, comment, fbn]])
-                    if len(phonies_arr) is 0:
-                        phonies_arr = temp_arr
-                    else:
-                        phonies_arr = np.concatenate([phonies_arr, temp_arr])
+                    # if this is a phony target, add to phonies
+                    comment = check_and_get_comment(i, "#?")
+                    phonies_arr = add_to_array(phonies_arr, [[target, comment, fbn]])
                 else:
-                    if "#>" in linewise[i-1]:
-                        comment = get_ml_comments(linewise, i, "#> ")
-                    else:
-                        comment = "No comment supplied."
+                    # if it's not phony, add to secondary targets
+                    comment = check_and_get_comment(i, "#>")
+                    targets = add_to_array(targets, [[target, comment, fbn]])
 
-                    if len(targets) is 0:
-                        targets = np.array([[target, comment, fbn]])
-                    else:
-                        targets = np.concatenate([targets, np.array([[target, comment, fbn]])])
 
 save_array(variables, "variables.txt")
 save_array(phonies_arr, "targets.txt")
